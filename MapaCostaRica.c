d5675bc843519b795ed201855b440c96c6fdc206
Aldokler <xdavidpastor@gmail.com>
2022-03-31 21:55:28 -0600

    Dibujo de heredia

---
+++ MapaCostaRica.c
@@ -0,0 +1,274 @@
+#include "malloc.h"
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#include <math.h>
+#include "MapaCostaRica.h"
+
+#define H_SIZE 600
+#define V_SIZE 600
+
+typedef struct {
+  double r;
+  double g;
+  double b;
+} COLOR;
+
+typedef struct {
+  int x;
+  int y;
+} PIX;
+
+void bressenham_line ();
+
+void plot();
+
+void paint_pol();
+
+void convert_utop();
+
+COLOR **buffer;
+
+/*
+#########################################################################################################################################
+-----------------------------------------------------------------------------------------------------------------------------------------
+#########################################################################################################################################
+*/
+
+int main(int argc, char** argv) 
+{
+  int i, j;
+
+  buffer = (COLOR **)malloc(H_SIZE * sizeof(COLOR*));
+  for (i = 0; i < H_SIZE; i++) 
+      {
+       buffer[i] = (COLOR *)malloc(V_SIZE * sizeof(COLOR));
+      }
+
+  for (i = 0; i < H_SIZE; i++) 
+      {
+       for (j = 0; j < V_SIZE; j++) 
+           {
+            buffer[i][j].r = 0;
+            buffer[i][j].g = 0;
+            buffer[i][j].b = 0;
+           }
+      }
+
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+  glutInitWindowSize(H_SIZE,V_SIZE);
+  glutCreateWindow("Una linea");
+  glClear(GL_COLOR_BUFFER_BIT);
+  gluOrtho2D(-0.5, H_SIZE +0.5, -0.5, V_SIZE + 0.5);
+  PIX* puntos = malloc(58 * sizeof(PIX));
+  printf("%Lf, %Lf \n", heredia[0]->x, heredia[57]->x);
+  convert_utop(heredia, puntos, 58);
+  paint_pol(puntos, 58);
+  printf("Listo! \n");
+  glutMainLoop();
+}
+
+/*
+#########################################################################################################################################
+-----------------------------------------------------------------------------------------------------------------------------------------
+#########################################################################################################################################
+*/
+
+void plot(int x,int y){
+  COLOR color;
+  buffer[x][y].r = 255;
+  buffer[x][y].g = 255;
+  buffer[x][y].b = 255;
+  glColor3f (buffer[x][y].r,buffer[x][y].g,buffer[x][y].b);
+  glBegin(GL_POINTS);
+  glVertex2i(x,y);
+  glEnd();
+  glFlush();
+}
+
+/*
+#########################################################################################################################################
+-----------------------------------------------------------------------------------------------------------------------------------------
+#########################################################################################################################################
+*/
+
+void paint_pol(PIX* apex_list, int n){
+  for (int i = 0; i < n-1; i++){
+    bressenham_line(apex_list[i].x, apex_list[i].y, apex_list[i+1].x, apex_list[i+1].y);
+  }
+  bressenham_line(apex_list[n-1].x, apex_list[n-1].y, apex_list[0].x, apex_list[0].y);
+}
+
+/*
+#########################################################################################################################################
+-----------------------------------------------------------------------------------------------------------------------------------------
+#########################################################################################################################################
+*/
+
+void convert_utop(UPOINT* apex_list[], PIX* points_list, int n){
+  int x,y;
+  for (int i = 0; i < n; i++){
+    x = trunc((apex_list[i]->x*H_SIZE)/len_x_universal);
+    y = trunc((apex_list[i]->y*V_SIZE)/len_y_universal);
+    //printf("%d, %d \n", x, y);
+    points_list[i].x = x; 
+    points_list[i].y = y;
+  }
+}
+
+/*
+#########################################################################################################################################
+-----------------------------------------------------------------------------------------------------------------------------------------
+#########################################################################################################################################
+*/
+
+void bressenham_line(int x0, int y0, int x1, int y1){
+  int Delta_E, Delta_NE, Delta_N, Delta_NO, x, xAbs, d;
+  int Delta_O, Delta_SO, Delta_S, Delta_SE, y, yAbs;
+
+  x = x0; y = y0;
+  plot(x, y);
+
+  xAbs = abs(x1 - x0);
+  yAbs = abs(y1 - y0);
+  
+  if (x0<=x1){
+    if (y0<=y1){
+      Delta_NE  =  2 * ((y1 - y0) - (x1 - x0)); //Valor para avanzar un pixel hacia arriba y uno a la derecha
+      if (xAbs>=yAbs){ //octante 1
+        d = 2*(y1-y0) - (x1-x0);
+        Delta_E   =  2 * (y1 - y0);                 //Valor para avanzar un pixel a la derecha
+        while (x < x1){ //escoge cual pixel pintar de acuerdo al signo de "d" hasta terminar la linea
+          if (d < 0){
+            x++;
+            d = d + Delta_E;
+          }
+          else{
+            x++; y++;
+            d = d + Delta_NE;
+          }
+          plot(x,y);
+        }
+      }
+      else{ //octante 2
+        d = (y1-y0) - 2*(x1-x0);
+        Delta_N  = -2 * (x1 - x0);             //Valor para avanzar un pixel hacia arriba
+        while (y < y1){ //escoge cual pixel pintar de acuerdo al signo de "d" hasta terminar la linea
+          if (d > 0){
+            y++;
+            d = d + Delta_N;
+          }
+          else{
+            x++; y++;
+            d = d + Delta_NE;
+          }
+          plot(x,y);
+        }
+      }
+    }
+    else{
+      Delta_SE  =  2 * ((y1 - y0) + (x1 - x0)); //Valor para avanzar un pixel hacia abajo y uno a la derecha
+      if (xAbs<=yAbs){ //octante 7
+        d = (y1-y0) + 2*(x1-x0);
+        Delta_S   =  2 * (x1 - x0);             //Valor para avanzar un pixel hacia abajo
+        while (y > y1){ //escoge cual pixel pintar de acuerdo al signo de "d" hasta terminar la linea
+          if (d < 0){
+            y--;
+            d = d + Delta_S;
+          }
+          else{
+            x++; y--;
+            d = d + Delta_SE;
+          }
+          plot(x,y);
+        }
+      }
+      else{ //octante 8
+        d = 2*(y1-y0) + (x1-x0);
+        Delta_E   =  2 * (y1 - y0);                 //Valor para avanzar un pixel a la derecha
+        while (x < x1){ //escoge cual pixel pintar de acuerdo al signo de "d" hasta terminar la linea
+          if (d >= 0){
+            x++;
+            d = d + Delta_E;
+          }
+          else{
+            x++; y--;
+            d = d + Delta_SE;
+          }
+          plot(x,y);
+        }
+      }
+    }
+  }
+  else{
+    if (y0<=y1){
+      Delta_NO  = -2 * ((y1 - y0) + (x1 - x0)); //Valor para avanzar un pixel hacia arriba y uno a la izquierda
+      if (yAbs>=xAbs){ //octante 3
+        d = -1*(y1-y0) - 2*(x1-x0);
+        Delta_N   = -2 * (x1 - x0);             //Valor para avanzar un pixel hacia arriba
+        while (y < y1){ //escoge cual pixel pintar de acuerdo al signo de "d" hasta terminar la linea
+          if (d <= 0){
+            y++;
+            d = d + Delta_N;
+          }
+          else{
+            x--; y++;
+            d = d + Delta_NO;
+          }
+          plot(x,y);
+        }
+      }    
+      else{ //octante 4
+        d = -2* (y1 - y0) - (x1 - x0);
+        Delta_O   = -2 * (y1 - y0);                 //Valor para avanzar un pixel a la izquierda
+        while (x > x1){ //escoge cual pixel pintar de acuerdo al signo de "d" hasta terminar la linea
+          if (d > 0){
+            x--;
+            d = d + Delta_O;
+          }
+          else{
+            x--; y++;
+            d = d + Delta_NO;
+          }
+          plot(x,y);
+        }
+      }
+    }
+    else{
+      Delta_SO  = -2 * ((y1 - y0) - (x1 - x0)); //Valor para avanzar un pixel hacia abajo y uno a la izquierda
+      if (xAbs>=yAbs){ //octante 5
+        d = -2* (y1 - y0) + (x1 - x0);
+        Delta_O   = -2 * (y1 - y0);                 //Valor para avanzar un pixel a la izquierda
+        while (x > x1){ //escoge cual pixel pintar de acuerdo al signo de "d" hasta terminar la linea
+          if (d <= 0){
+            x--;
+            d = d + Delta_O;
+          }
+          else{
+            x--; y--;
+            d = d + Delta_SO;
+          }
+          plot(x,y);
+        }
+      }
+      else{ //octante 6
+        d = -1*(y1 - y0) + 2 * (x1 - x0);
+        Delta_S   =  2 * (x1 - x0);               //Valor para avanzar un pixel hacia abajo
+        while (y > y1){ //escoge cual pixel pintar de acuerdo al signo de "d" hasta terminar la linea
+          if (d >= 0){
+            y--;
+            d = d + Delta_S;
+          }
+          else{
+            x--; y--;
+            d = d + Delta_SO;
+          }
+          plot(x,y);
+        }
+      }
+    }
+  }
+};
+
---
+++ MapaCostaRica.h
@@ -0,0 +1,77 @@
+int len_x_universal = 28.41;
+int len_y_universal = 26.8;
+
+typedef struct {
+  long double x;
+  long double y;
+} UPOINT;
+
+//HEREDIA
+	UPOINT hrd_0 = {.x = 14.83, .y = 22.5};
+	UPOINT hrd_1 = {.x = 14.95, .y = 22.37};
+	UPOINT hrd_2 = {.x = 15.13, .y = 22.43};
+	UPOINT hrd_3 = {.x = 15.3, .y = 22.32};
+	UPOINT hrd_4 = {.x = 15.489, .y = 22.36};
+	UPOINT hrd_5 = {.x = 15.51, .y = 22.48};
+	UPOINT hrd_6 = {.x = 15.69, .y = 22.41};
+	UPOINT hrd_7 = {.x = 15.74, .y = 22.57};
+	UPOINT hrd_8 = {.x = 15.84, .y = 22.4};
+	UPOINT hrd_9 = {.x = 16.13, .y = 22.25};
+	UPOINT hrd_10 = {.x = 16.2, .y = 22.1};
+	UPOINT hrd_11 = {.x = 16.44, .y = 22.02};
+	UPOINT hrd_12 = {.x = 16.5, .y = 21.98};
+	UPOINT hrd_13 = {.x = 16.82, .y = 22.05};
+	UPOINT hrd_14 = {.x = 17, .y = 22.001};
+	UPOINT hrd_15 = {.x = 17.21, .y = 22.24};
+	UPOINT hrd_16 = {.x = 17.39, .y = 22.23};
+	UPOINT hrd_17 = {.x = 17.6, .y = 22.44};
+	UPOINT hrd_18 = {.x = 17.87, .y = 22.43}; //Limon
+	UPOINT hrd_19 = {.x = 18.001, .y = 22.3};
+	UPOINT hrd_20 = {.x = 17.998, .y = 22.089}; 
+	UPOINT hrd_21 = {.x = 18.09, .y = 22};
+	UPOINT hrd_22 = {.x = 18.2, .y = 21.91};
+	UPOINT hrd_23 = {.x = 17.93, .y = 21.9};
+	UPOINT hrd_24 = {.x = 17.8, .y = 21.91};
+	UPOINT hrd_25 = {.x = 17.7, .y = 21.88};
+	UPOINT hrd_26 = {.x = 17.51, .y = 21.8};
+	UPOINT hrd_27 = {.x = 17.38, .y = 21.6};
+	UPOINT hrd_28 = {.x = 17.23, .y = 21.57};
+	UPOINT hrd_29 = {.x = 17.2, .y = 21.17};
+	UPOINT hrd_30 = {.x = 17.27, .y = 21.08};
+	UPOINT hrd_31 = {.x = 17.18, .y = 20.77};
+	UPOINT hrd_32 = {.x = 17.31, .y = 20.34};
+	UPOINT hrd_33 = {.x = 17.21, .y = 20};
+	UPOINT hrd_34 = {.x = 17.07, .y = 19.91};
+	UPOINT hrd_35 = {.x = 16.9, .y = 19.59};
+	UPOINT hrd_36 = {.x = 17.15, .y = 19.37};
+	UPOINT hrd_37 = {.x = 17.175, .y = 19.08};
+	UPOINT hrd_38 = {.x = 17.15, .y = 18.89};
+	UPOINT hrd_39 = {.x = 17.175, .y = 18.615};
+	UPOINT hrd_40 = {.x = 17.05, .y = 18.4};
+	UPOINT hrd_41 = {.x = 16.95, .y = 18.38};
+	UPOINT hrd_42 = {.x = 16.82, .y = 18.2};
+	UPOINT hrd_43 = {.x = 16.675, .y = 18.16};
+	UPOINT hrd_44 = {.x = 16.52, .y = 17.98}; //Limon
+	UPOINT hrd_45 = {.x = 16.23, .y = 17.77};
+	UPOINT hrd_46 = {.x = 16.2, .y = 17.46};
+	UPOINT hrd_47 = {.x = 16.1, .y = 17.23};
+	UPOINT hrd_48 = {.x = 15.85, .y = 17.15};
+	UPOINT hrd_49 = {.x = 15.81, .y = 16.945};
+	UPOINT hrd_50 = {.x = 15.95, .y = 16.84};
+	UPOINT hrd_51 = {.x = 15.9, .y = 16.6};
+	UPOINT hrd_52 = {.x = 15.67, .y = 16.32};
+	UPOINT hrd_53 = {.x = 15.3, .y = 16.19};
+	UPOINT hrd_54 = {.x = 15.1, .y = 16.22};
+	UPOINT hrd_55 = {.x = 14.9, .y = 16.189};
+	UPOINT hrd_56 = {.x = 14.65, .y = 16.27};
+	UPOINT hrd_57 = {.x = 14.82, .y = 17.2};
+
+//HEREDIA
+UPOINT * heredia[58] = {
+	&hrd_0, &hrd_1, &hrd_2, &hrd_3, &hrd_4, &hrd_5, &hrd_6, &hrd_7, &hrd_8, &hrd_9,
+	&hrd_10, &hrd_11, &hrd_12, &hrd_13, &hrd_14, &hrd_15, &hrd_16, &hrd_17, &hrd_18, &hrd_19,
+	&hrd_20, &hrd_21, &hrd_22, &hrd_23, &hrd_24, &hrd_25, &hrd_26, &hrd_27, &hrd_28, &hrd_29,
+	&hrd_30, &hrd_31, &hrd_32, &hrd_33, &hrd_34, &hrd_35, &hrd_36, &hrd_37, &hrd_38, &hrd_39,
+	&hrd_40, &hrd_41, &hrd_42, &hrd_43, &hrd_44, &hrd_45, &hrd_46, &hrd_47, &hrd_48, &hrd_49,
+	&hrd_50, &hrd_51, &hrd_52, &hrd_53, &hrd_54, &hrd_55, &hrd_56, &hrd_57
+};
